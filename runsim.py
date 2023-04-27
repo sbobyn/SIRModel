fcb64bf1001c6c6fc351bd3e5c5f76aed8b01e53
Steven Bobyn <stevenbobyn@icloud.com>
2023-04-26 21:11:27 -0700

    finished sir sim and plot

--- runsim.py
+++ runsim.py
@@ -1,50 +1,101 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 from sir import *
-import numpy as np
 import time
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.patches import Circle
 
+
 boundary = 10
-num_agents = 100
-num_iter = 1000
+num_agents = 200
+num_iter = 200
 dt = 1/50 # 50 Hz
+infection_radius = 0.5
+infection_probability = 0.2
+infection_duration = 30
 
 print("Running simulation...")
 start_time = time.time()
-sim = SIR(boundary,num_agents,num_iter,dt)
+sim = SIR(
+    boundary=boundary,
+    num_agents=num_agents,
+    num_iter=num_iter,
+    dt=dt,
+    infection_radius=infection_radius,
+    infection_probability=infection_probability,
+    infection_duration=infection_duration)
 sim.run()
 end_time = time.time()
 elapsed_time = end_time - start_time
 print("Total sim time\t{:.2f} seconds".format(elapsed_time))
 
 fig_size=6
-fig, ax = plt.subplots(figsize=(fig_size, fig_size))
-ax.set_xlim(-boundary, boundary)
-ax.set_ylim(-boundary, boundary)
-ax.set_title(f"Step 0/{num_iter}")
+fig, (graph_ax,sim_ax) = plt.subplots(1,2,figsize=(fig_size*2, fig_size))
+
+time_steps = list(range(num_iter))
+sus_history,inf_history,rem_history = sim.get_history()
+
+color_map = {
+    State.SUS : 'blue',
+    State.INF : 'red',
+    State.REM : 'grey',
+}
+
+def setup_graph(ax):
+    ax.set_xlim(0,num_iter-1)
+    ax.set_ylim(0,num_agents)
+    colors = ['blue', 'red', 'gray']
+    labels = ['Susceptible', 'Infected', 'Recovered'] 
+    legend_elements = [plt.Rectangle((0,0), 1, 1, color=color) for color in colors]
+    ax.legend(legend_elements, labels, loc='upper left',facecolor='white')
+    ax.set_title("SIR Plot")
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+setup_graph(graph_ax)
+
+sim_ax.set_xlim(-boundary, boundary)
+sim_ax.set_ylim(-boundary, boundary)
+sim_ax.set_title(f"Susceptible: {sus_history[0]} Infected: {inf_history[0]} Recovered: {rem_history[0]}")
 x = [agent.position_history[0][0] for agent in sim.agents]
 y = [agent.position_history[0][1] for agent in sim.agents]
+state_history = [agent.state_history for agent in sim.agents]
 point_size = 0.1
-# Create a scatter plot with the specified point size
-circles = [Circle((x[i], y[i]), point_size, edgecolor='black', facecolor='black') for i in range(num_agents)]
-for circle in circles:
-    ax.add_patch(circle)
+sim_ax.set_xticks([])
+sim_ax.set_yticks([])
 
-def animate(i):
-    ax.set_title(f"Step {i}/{num_iter}")
-    x = [agent.position_history[i][0] for agent in sim.agents]
-    y = [agent.position_history[i][1] for agent in sim.agents]
+# Create a scatter plot with the specified point size
+circles = [Circle((x[i], y[i]), point_size, edgecolor=color_map[state_history[i][0]], facecolor=color_map[state_history[i][0]]) for i in range(num_agents)]
+for circle in circles:
+    sim_ax.add_patch(circle)
+
+def animate(step):
+    if step == 0:
+        graph_ax.clear()
+        setup_graph(graph_ax)
+
+    inf_count = inf_history[step]
+    rem_count = rem_history[step]
+    sus_count = sus_history[step]
+
+    # Add a new bar for the current state of each group (S, I, R)
+    graph_ax.bar(step, inf_count, color=color_map[State.INF], width=1, align='edge')
+    graph_ax.bar(step, rem_count, bottom=inf_count, color=color_map[State.REM], width=1, align='edge')
+    graph_ax.bar(step, sus_count, bottom=inf_count + rem_count, color=color_map[State.SUS], width=1, align='edge')
+    
+    sim_ax.set_title(f"Susceptible: {sus_history[step]} Infected: {inf_history[step]} Recovered: {rem_history[step]}")
+    x = [agent.position_history[step][0] for agent in sim.agents]
+    y = [agent.position_history[step][1] for agent in sim.agents]
     for i in range(num_agents):
         circles[i].center = (x[i], y[i])
+        circles[i].set_edgecolor(color_map[state_history[i][step]])
+        circles[i].set_facecolor(color_map[state_history[i][step]])
 
 sim_speed = 2
-frames = sim.num_iter
 interval = (1000*sim.dt)/sim_speed
 
-ani = animation.FuncAnimation(fig, animate, frames=frames, interval=interval, blit=False)
+ani = animation.FuncAnimation(fig, animate, frames=sim.num_iter, interval=interval, blit=False)
 
 # code to toggle animation
 ani_paused = False
--- sir.py
+++ sir.py
@@ -1,5 +1,7 @@
 from enum import Enum
 import numpy as np
+from scipy.spatial import distance
+from scipy.spatial.distance import pdist, squareform
 
 class State(Enum):
     SUS = 0 # susceptible
@@ -12,33 +14,106 @@ class Agent:
         self.pos = np.random.uniform(-boundary,boundary,size=2)
         self.vel = np.random.uniform(-1,1,size=2)
         self.state = state
+        self.new_state = None
         self.position_history = []
+        self.state_history = []
+        self.infected_neighbours = []
+        self.time_infected = 0
 
     def update_position(self,dt):
         self.position_history.append(self.pos)
         noise = np.random.uniform(-1,1,size=2)*0.1
         self.vel = np.clip( self.vel+noise,-1,1 )
-        pos = self.pos + self.vel*dt
+        pos = self.pos
         # keep in bounds
-        if pos[0] < -self.boundary: pos[0] = self.boundary
-        if pos[0] > self.boundary: pos[0] = -self.boundary
-        if pos[1] < -self.boundary: pos[1] = self.boundary
-        if pos[1] > self.boundary: pos[1] = -self.boundary
+        if pos[0] < -self.boundary: self.vel[0] = - self.vel[0]
+        if pos[0] > self.boundary: self.vel[0] = - self.vel[0]
+        if pos[1] < -self.boundary: self.vel[1] = - self.vel[1]
+        if pos[1] > self.boundary: self.vel[1] = - self.vel[1]
+        pos = self.pos + self.vel*dt
         self.pos = pos
 
-    def update_state_to(self,new_state):
-        self.state = new_state
+    def update_state(self):
+        self.state_history.append(self.state)
+        if self.new_state is None:
+            return
+        self.state = self.new_state
+        self.new_state = None
 
 class SIR:
-    def __init__(self,boundary,num_agents,num_iter,dt):
-        self.agents = [ Agent(boundary,dt) for _ in range(num_agents) ]
+    def __init__(
+            self,
+            boundary,
+            num_agents,
+            num_iter,
+            dt,
+            infection_radius,
+            infection_probability,
+            infection_duration):
+        self.agents = [ Agent(boundary) for _ in range(num_agents) ]
+        self.agents[0].new_state = State.INF # patient zero
         self.num_agents = num_agents
         self.num_iter = num_iter
         self.dt = dt
+        self.inf_rad = infection_radius
+        self.inf_p = infection_probability
+        self.inf_duration = infection_duration
+        self.num_sus = num_agents-1
+        self.num_inf = 1
+        self.num_rem = 0
+        self.sus_history = []
+        self.inf_history = []
+        self.rem_history = []
+    
+    def get_history(self):
+        return self.sus_history,self.inf_history,self.rem_history
+
+    def get_neighbors(self, current_point):
+        agent_points = [agent.pos for agent in self.agents]
+        distances = distance.cdist([current_point], agent_points)
+        nearby = distances < self.inf_rad
+        self_point = distances == 0
+        self_point_index = np.nonzero(self_point)
+        nearby[self_point_index] = False
+        nearby = nearby[0]
+        infected_neighbours = [ self.agents[idx] for idx,bool in enumerate(nearby) if bool == True ]
+        return infected_neighbours
+
+    def update_neighbors(self):
+        for agent in self.agents:
+            agent.infected_neighbours = self.get_neighbors(agent.pos)
+
+    def update_states(self):
+        for agent in self.agents:
+            if agent.state == State.SUS:
+                infect_threshold = self.inf_p * len(agent.infected_neighbours)
+                infect_likelihood = np.random.random() # rand num between 0 and 1
+                if infect_likelihood < infect_threshold:
+                    agent.new_state = State.INF
+                    self.num_sus -= 1
+                    self.num_inf += 1
+            elif agent.state == State.INF:
+                agent.time_infected += 1
+                if agent.time_infected > self.inf_duration:
+                    agent.new_state = State.REM
+                    self.num_inf -= 1
+                    self.num_rem += 1
+        # update states to new states
+        for agent in self.agents:
+            agent.update_state()
+
+    def update_history(self):
+        self.sus_history.append(self.num_sus)
+        self.inf_history.append(self.num_inf)
+        self.rem_history.append(self.num_rem)
 
     def step(self):
+        self.update_neighbors()
         for agent in self.agents:
             agent.update_position(self.dt)
+        # # record SIR history
+        self.update_states()
+        self.update_history()
 
     def run(self):
         for _ in range(self.num_iter):
